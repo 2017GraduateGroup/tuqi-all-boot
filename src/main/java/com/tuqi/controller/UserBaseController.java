b241ec941d733acd3e0141c08284dc051323fa91
KarryCol <1003478213@qq.com>
2017-06-03 16:14:03 +0800

    bugfix: 登录接口修改

--- src/main/java/com/tuqi/controller/UserBaseController.java
+++ src/main/java/com/tuqi/controller/UserBaseController.java
@@ -4,12 +4,14 @@ import com.tuqi.domain.model.UserDO;
 import com.tuqi.domain.query.UserQuery;
 import com.tuqi.manager.UserManager;
 import com.tuqi.util.MyMD5Util;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +26,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("user")
+@Slf4j
 public class UserBaseController {
     @Autowired
     UserManager userManager;
@@ -50,22 +53,54 @@ public class UserBaseController {
         userManager.insertSelective(userDO);
         return "/loginAndRegister";
     }
+
     //用户登录
+    @ResponseBody
     @RequestMapping("userLogin")
-    public String userLogin(@RequestParam String usernameInput, @RequestParam String passwdInput, HttpServletRequest request, Model model){
+    public Integer userLogin(@RequestParam String usernameInput, @RequestParam String passwdInput, HttpServletRequest request){
         if(StringUtils.isNotBlank(usernameInput)){
             UserQuery userQuery = new UserQuery();
-            userQuery.createCriteria().andUserNameEqualTo(usernameInput);
+            userQuery.createCriteria().andUserNameEqualTo(usernameInput).andPasswordEqualTo(passwdInput);
             List<UserDO> userDOList = userManager.selectByQuery(userQuery);
-            model.addAttribute("currentUser", userDOList.get(0));
-            request.getSession().setAttribute("currentUserId", userDOList.get(0).getUserId());
-            if(StringUtils.isNotBlank(passwdInput)){
-                String mdPwd = MyMD5Util.code(passwdInput);
-                if(userDOList.get(0).getPassword().equals(mdPwd)){
-                    return "/index";
-                }
+            if(userDOList.size() > 0){
+                request.getSession().setAttribute("currentUserId", userDOList.get(0).getUserId());
+                return 1;
+            }else{
+                return 0;
             }
         }
-        return "";
+        return 0;
+    }
+
+    //获取所有的用户信息
+    @ResponseBody
+    @RequestMapping("getAllUserInfo")
+    public List getAllUserInfo(){
+        UserQuery userQuery = new UserQuery();
+        userQuery.createCriteria().andUserIdIsNotNull();
+        List<UserDO> userDOList = userManager.selectByQuery(userQuery);
+        return userDOList;
+    }
+
+    //修改用户信息
+    @RequestMapping("updateUserInfo")
+    public void updateUserInfo(@RequestParam String userId,String userName, String nickName, String password, String userType){
+        if(StringUtils.isNotBlank(userId)){
+            UserDO userDO = userManager.selectByPrimaryKey(Long.valueOf(userId));
+            if(userDO != null){
+                if(StringUtils.isNotBlank(userName)){
+                    userDO.setUserName(userName);
+                }
+                if(StringUtils.isNotBlank(nickName)){
+                    userDO.setUserNickName(nickName);
+                }
+                if(StringUtils.isNotBlank(password)){
+                    userDO.setPassword(password);
+                }
+                log.info("update user info success", userId);
+            }
+        }else {
+            log.info("user id is null", userId);
+        }
     }
 }
